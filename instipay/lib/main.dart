4ddd6223e24977af05646ffe5d7e6da899128124
tusharb <tusharbansal@iitbhilai.ac.in>
2023-01-15 12:49:49 +0530

    added logout button

--- instipay/lib/main.dart
+++ instipay/lib/main.dart
@@ -102,6 +102,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       routerConfig: _router,
     );
   }
